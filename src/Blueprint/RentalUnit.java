9323f1ff49bc09220a29fe3a6adec615d47600b4
hdeep <hdeep@LAPTOP-81DUFOAI.wireless.concordia.ca>
2023-03-04 16:35:03 -0500

    Adding getter and setters

--- src/Blueprint/RentalUnit.java
+++ src/Blueprint/RentalUnit.java
@@ -16,5 +16,85 @@ public class RentalUnit {
 		this.city = city;
 		this.zipCode = zipCode;
 	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getTenantId() {
+		return tenantId;
+	}
+
+	public void setTenantId(String tenantId) {
+		this.tenantId = tenantId;
+	}
+
+	public String getLeaseId() {
+		return leaseId;
+	}
+
+	public void setLeaseId(String leaseId) {
+		this.leaseId = leaseId;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getBedrooms() {
+		return bedrooms;
+	}
+
+	public void setBedrooms(String bedrooms) {
+		this.bedrooms = bedrooms;
+	}
+
+	public String getBathroom() {
+		return bathroom;
+	}
+
+	public void setBathroom(String bathroom) {
+		this.bathroom = bathroom;
+	}
+
+	public String getSqft() {
+		return sqft;
+	}
+
+	public void setSqft(String sqft) {
+		this.sqft = sqft;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getZipCode() {
+		return zipCode;
+	}
+
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+
+	public String getRuId() {
+		return ruId;
+	}
+
+	public void setRuId(String ruId) {
+		this.ruId = ruId;
+	}
 	
 }
