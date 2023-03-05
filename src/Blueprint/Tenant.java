aac951f61ff1a231a79279bd99e4688d53eadab8
PreetAngadSingh <preetangad107@gmail.com>
2023-03-04 19:12:25 -0500

    implemented tenant class

--- src/Blueprint/Tenant.java
+++ src/Blueprint/Tenant.java
@@ -4,15 +4,21 @@ import java.util.HashMap;
 public class Tenant {
 	String tenantID; 
 	String fullName;
+	String phoneNumber;
+	String email;
 	String tenantStatus;
 	String ruId; 
 	String leaseID;
-	
+
 	HashMap<String, String> payments = new HashMap<String, String>();
 	
-	public Tenant(String tenantID, String fullName, String tenantStatus, String ruId, String leaseID) {
+	public Tenant(String tenantID, String fullName, String phoneNumber, String email, String tenantStatus, String ruId,
+			String leaseID) {
+		super();
 		this.tenantID = tenantID;
 		this.fullName = fullName;
+		this.phoneNumber = phoneNumber;
+		this.email = email;
 		this.tenantStatus = tenantStatus;
 		this.ruId = ruId;
 		this.leaseID = leaseID;
@@ -32,6 +38,22 @@ public class Tenant {
 
 	public void setFullName(String fullName) {
 		this.fullName = fullName;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
 	}
 
 	public String getTenantStatus() {
---
+++ src/TenantClass.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+import Blueprint.Tenant;
+
+public class TenantClass {
+	public static Tenant addTenant(int size) {
+		Scanner addATenantSc = new Scanner(System.in);
+		String tenantID = String.valueOf(size+1); 
+		String fullName;
+		String phoneNumber;
+		String email;
+		
+		// set fullName 
+		System.out.print("Enter tenant's Full Name : ");
+		fullName = addATenantSc.nextLine();
+		System.out.println();
+		
+		// set phoneNumber 
+		System.out.print("Enter tenant's Phone Number : ");
+		phoneNumber = addATenantSc.nextLine();
+		System.out.println();
+		
+		// set email 
+		System.out.print("Enter tenant's email : ");
+		email = addATenantSc.nextLine();
+		System.out.println();
+				
+		Tenant newTenant = new Tenant(tenantID,fullName, phoneNumber, email, "invalid", "null", "null");
+		
+		return newTenant;
+	}
+	
+	public static void displayTenant(ArrayList<Tenant> tenants) {
+		for(Tenant tenant : tenants) {
+			System.out.println("Tenant ID : " + tenant.getTenantID());
+			System.out.println("Tenant Full Name : " + tenant.getFullName());
+			System.out.println("Tenant Phone Number : " + tenant.getPhoneNumber());
+			System.out.println("Tenant Email : " + tenant.getEmail());
+			System.out.println("Tenant Status : " + tenant.getTenantStatus());
+			System.out.println("Tenant Rental Unit : " + tenant.getRuId());
+			System.out.println("Tenant Lease ID : " + tenant.getLeaseID());
+		}
+	}
+}
